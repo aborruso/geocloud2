a8f5d25a5fcce2dc1287a4a82e871f84819a0345
Martin Høgh <mh@mapcentia.com>
2014-08-05 19:19:57 +0200

    no message

--- app/conf/migration/Sql.php
+++ app/conf/migration/Sql.php
@@ -9,55 +9,39 @@ class Sql
         $sqls[] = "ALTER TABLE settings.geometry_columns_join ADD COLUMN bitmapsource VARCHAR(255)";
         $sqls[] = "ALTER TABLE settings.geometry_columns_join ADD CONSTRAINT geometry_columns_join_key UNIQUE (_key_)";
         $sqls[] = "ALTER TABLE settings.geometry_columns_join ALTER data TYPE TEXT";
-        $sqls[] = "ALTER TABLE settings.geometry_columns_join ALTER sort_id SET DEFAULT 0";
-        $sqls[] = "UPDATE settings.geometry_columns_join SET sort_id = 0 WHERE sort_id IS NULL";
-
-        $sqls[] = "CREATE VIEW settings.geometry_columns_view_vector AS
+        $sqls[] = "ALTER TABLE settings.geometry_columns_join ADD COLUMN privileges TEXT";
+        $sqls[] = "ALTER TABLE settings.geometry_columns_join ALTER sort_id set default 0";
+        $sqls[] = "CREATE EXTENSION \"uuid-ossp\"";
+        $sqls[] = "CREATE VIEW settings.geometry_columns_view_base AS
                       SELECT
                         geometry_columns.f_table_schema,
                         geometry_columns.f_table_name,
                         geometry_columns.f_geometry_column,
                         geometry_columns.coord_dimension,
                         geometry_columns.srid,
-                        geometry_columns.type,
-
-                        geometry_columns_join._key_,
-                        geometry_columns_join.f_table_abstract,
-                        geometry_columns_join.f_table_title,
-                        geometry_columns_join.tweet,
-                        geometry_columns_join.editable,
-                        geometry_columns_join.created,
-                        geometry_columns_join.lastmodified,
-                        geometry_columns_join.authentication,
-                        geometry_columns_join.fieldconf,
-                        geometry_columns_join.meta_url,
-                        geometry_columns_join.layergroup,
-                        geometry_columns_join.def,
-                        geometry_columns_join.class,
-                        geometry_columns_join.wmssource,
-                        geometry_columns_join.baselayer,
-                        geometry_columns_join.sort_id,
-                        geometry_columns_join.tilecache,
-                        geometry_columns_join.data,
-                        geometry_columns_join.not_querable,
-                        geometry_columns_join.single_tile,
-                        geometry_columns_join.cartomobile,
-                        geometry_columns_join.filter,
-                        geometry_columns_join.bitmapsource
+                        geometry_columns.type
                       FROM geometry_columns
-                        LEFT JOIN
-                        settings.geometry_columns_join ON
-                                                         geometry_columns.f_table_schema || '.' || geometry_columns.f_table_name || '.' || geometry_columns.f_geometry_column::text =
-                                                         geometry_columns_join._key_::text";
 
-        $sqls[] = "CREATE VIEW settings.geometry_columns_view_raster AS
+                      UNION ALL
                       SELECT
                         raster_columns.r_table_schema as f_table_schema,
                         raster_columns.r_table_name as f_table_name,
                         raster_columns.r_raster_column as f_geometry_column,
                         2 as coord_dimension,
                         raster_columns.srid,
-                        'RASTER' as type,
+                        'RASTER' as type
+                      FROM raster_columns
+
+
+                    ";
+        $sqls[] = "CREATE VIEW settings.geometry_columns_view AS
+                      SELECT
+                        geometry_columns_view_base.f_table_schema,
+                        geometry_columns_view_base.f_table_name,
+                        geometry_columns_view_base.f_geometry_column,
+                        geometry_columns_view_base.coord_dimension,
+                        geometry_columns_view_base.srid,
+                        geometry_columns_view_base.type,
 
                         geometry_columns_join._key_,
                         geometry_columns_join.f_table_abstract,
@@ -82,17 +66,11 @@ class Sql
                         geometry_columns_join.cartomobile,
                         geometry_columns_join.filter,
                         geometry_columns_join.bitmapsource
-                      FROM raster_columns
+                      FROM settings.geometry_columns_view_base
                         LEFT JOIN
                         settings.geometry_columns_join ON
-                                                         raster_columns.r_table_schema || '.' || raster_columns.r_table_name || '.' || raster_columns.r_raster_column::text =
-                                                         geometry_columns_join._key_::text;
-                    ";
-
-        $sqls[] = "CREATE VIEW settings.geometry_columns_view AS
-                      SELECT * FROM settings.geometry_columns_view_vector
-                        UNION ALL
-                      SELECT * FROM settings.geometry_columns_view_raster";
+                                                         settings.geometry_columns_view_base.f_table_schema || '.' || settings.geometry_columns_view_base.f_table_name || '.' || settings.geometry_columns_view_base.f_geometry_column::text =
+                                                         geometry_columns_join._key_::text";
         return $sqls;
     }
 }
